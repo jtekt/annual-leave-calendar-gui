febd4b3c2fdf0f302e90eb050334be48ffef6415
Maxime Moreillon <m.moreillon@gmail.com>
2022-05-23 11:42:20 +0900

    deleted home.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
   {
     path: '/about',
     name: 'about',
@@ -27,6 +21,7 @@ const routes = [
   },
   {
     path: '/entries/new',
+    alias: '/',
     name: 'new_entry',
     component: () => import('../views/NewEntry.vue')
   },
