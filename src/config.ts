f7292b34c436cda01d60a5a107a8512f3fca6357
Maxime Moreillon <m.moreillon@gmail.com>
2026-04-14 22:24:21 +0900

    removed unused field in config

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,10 @@
 export const colors = {
   leaves: {
     taken: "#666666",
-    future: "#777777",
+    future: "#888888",
   },
   allocations: {
     current_year_grants: "#a0d1e7",
     carried_over: "#6fb6da",
   },
 }
-
-export const notTakenOpacity = 0.5
