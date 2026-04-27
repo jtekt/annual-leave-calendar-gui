e471a9c049420acf4fbe90396cbf6d557e4029b3
Maxime Moreillon <m.moreillon@gmail.com>
2026-04-28 08:55:38 +0900

    VITE_APPS_URL

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,7 +1,11 @@
 /* eslint-disable */
 declare module "*.vue" {
   import type { DefineComponent } from "vue"
-  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>
+  const component: DefineComponent<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    unknown
+  >
   export default component
 }
 
@@ -10,7 +14,7 @@ interface ImportMetaEnv {
   readonly VITE_LOGIN_URL?: string
   readonly VITE_IDENTIFICATION_URL?: string
   readonly VITE_LOGIN_HINT?: string
-  readonly VITE_HOMEPAGE_URL?: string
+  readonly VITE_APPS_URL?: string
   readonly VITE_USER_MANAGER_API_URL?: string
   readonly VITE_GROUP_MANAGER_API_URL?: string
   readonly VITE_AUTHENTICATION_API_URL?: string
